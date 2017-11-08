223e02901bf8324a9fd571021f3fd368dad45f72
Helena <helena.wentworth@gmail.com>
2017-11-07 21:12:26 -0500

    Add dummy questionnaire form

---
+++ resources/views/daily-qs.blade.php
@@ -0,0 +1,47 @@
+@section('content')
+    <h1 class="text-center">
+        Hey (user?) -- quick question for you
+    </h1>
+
+    <form>
+        <h3>How did you sleep last night?</h3>
+        <div class="form-group">
+            <label for="sleepTime">I slept for...</label>
+            <select class="form-control" id="sleepTime">
+                <option selected disabled hidden>----</option>
+                <option>less than 4 hours</option>
+                <option>between 4 and 6 hours</option>
+                <option>between 6 and 8 hours</option>
+                <option>more than 8 hours</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="sleepQuality">I would say my sleep quality was...</label>
+            <select class="form-control" id="sleepQuality">
+                <option selected disabled hidden>----</option>
+                <option>excellent</option>
+                <option>good</option>
+                <option>fine</option>
+                <option>bad</option>
+                <option>terrible</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="sleepWakingUp">Waking up was...</label>
+            <select class="form-control" id="sleepWakingUp">
+                <option selected disabled hidden>----</option>
+                <option>very easy</option>
+                <option>easy</option>
+                <option>fine</option>
+                <option>difficult</option>
+                <option>very difficult</option>
+            </select>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+@endsection
+
+@extends('partials/layout')
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('daily-qs', function () {
+    return view('daily-qs');
+});
