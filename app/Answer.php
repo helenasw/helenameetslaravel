160807588dab82c719cc1a0092c1fe20c8ce49ff
Helena <helena.wentworth@gmail.com>
2017-11-08 00:48:15 -0500

    Add values and names to form inputs that correspond with predefined question and answer type string identifiers

--- app/Answer.php
+++ app/Answer.php
@@ -4,5 +4,45 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * User answers to our predefined questions.
+ */
 class Answer extends Model {
+
+    /**
+     * In a real application, this relationship (and predefined answers)
+     * should be stored in the database.
+     *
+     * @param string $question_type
+     * @return string[]
+     */
+    public static function getValidAnswersForQuestionType(string $question_type): array {
+        switch ($question_type) {
+            case Question::TYPE_SLEEP_TIME:
+                return [
+                    'answer-sleep-time-less-4',
+                    'answer-sleep-time-4-to-6',
+                    'answer-sleep-time-6-to-8',
+                    'answer-sleep-time-8-plus'
+                ];
+            case Question::TYPE_SLEEP_QUALITY:
+                return [
+                    'answer-sleep-quality-excellent',
+                    'answer-sleep-quality-good',
+                    'answer-sleep-quality-fine',
+                    'answer-sleep-quality-bad',
+                    'answer-sleep-quality-terrible',
+                ];
+            case Question::TYPE_DIFFICULTY_WAKING_UP:
+                return [
+                    'answer-difficulty-waking-up-very-easy',
+                    'answer-difficulty-waking-up-easy',
+                    'answer-difficulty-waking-up-fine',
+                    'answer-difficulty-waking-up-difficult',
+                    'answer-difficulty-waking-up-very-difficult',
+                ];
+            default:
+                return [];
+        }
+    }
 }
--- resources/views/daily-qs.blade.php
+++ resources/views/daily-qs.blade.php
@@ -8,36 +8,36 @@
         <div class="form-group">
             {{ csrf_field() }}
             <label for="sleepTime">I slept for...</label>
-            <select class="form-control" id="sleepTime">
+            <select class="form-control" id="sleepTime" name="question_type_sleep_time">
                 <option selected disabled hidden>----</option>
-                <option>less than 4 hours</option>
-                <option>between 4 and 6 hours</option>
-                <option>between 6 and 8 hours</option>
-                <option>more than 8 hours</option>
+                <option value="answer-sleep-time-less-4">less than 4 hours</option>
+                <option value="answer-sleep-time-4-to-6">between 4 and 6 hours</option>
+                <option value="answer-sleep-time-6-to-8">between 6 and 8 hours</option>
+                <option value="answer-sleep-time-8-plus">more than 8 hours</option>
             </select>
         </div>
 
         <div class="form-group">
             <label for="sleepQuality">I would say my sleep quality was...</label>
-            <select class="form-control" id="sleepQuality">
+            <select class="form-control" id="sleepQuality" name="question_type_sleep_quality">
                 <option selected disabled hidden>----</option>
-                <option>excellent</option>
-                <option>good</option>
-                <option>fine</option>
-                <option>bad</option>
-                <option>terrible</option>
+                <option value="answer-sleep-quality-excellent">excellent</option>
+                <option value="answer-sleep-quality-good">good</option>
+                <option value="answer-sleep-quality-fine">fine</option>
+                <option value="answer-sleep-quality-bad">bad</option>
+                <option value="answer-sleep-quality-terrible">terrible</option>
             </select>
         </div>
 
         <div class="form-group">
-            <label for="sleepWakingUp">Waking up was...</label>
-            <select class="form-control" id="sleepWakingUp">
+            <label for="difficultyWakingUp">Waking up was...</label>
+            <select class="form-control" id="difficultyWakingUp" name="question_type_difficulty_waking_up">
                 <option selected disabled hidden>----</option>
-                <option>very easy</option>
-                <option>easy</option>
-                <option>fine</option>
-                <option>difficult</option>
-                <option>very difficult</option>
+                <option value="answer-difficulty-waking-up-very-easy">very easy</option>
+                <option value="answer-difficulty-waking-up-easy">easy</option>
+                <option value="answer-difficulty-waking-up-fine">fine</option>
+                <option value="answer-difficulty-waking-up-difficult">difficult</option>
+                <option value="answer-difficulty-waking-up-very-difficult">very difficult</option>
             </select>
         </div>
 
