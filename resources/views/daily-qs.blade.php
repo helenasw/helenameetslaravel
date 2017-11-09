ae83713d82d22b49d607b14b66f19b9baed23aca
Helena <helena.wentworth@gmail.com>
2017-11-08 22:35:18 -0500

    CLient side form validation for the daily qs

--- resources/views/daily-qs.blade.php
+++ resources/views/daily-qs.blade.php
@@ -13,8 +13,8 @@
                         <div class="form-group">
                             {{ csrf_field() }}
                             <label for="sleepTime">I slept for...</label>
-                            <select class="form-control" id="sleepTime" name="question_type_sleep_time">
-                                <option selected disabled hidden>----</option>
+                            <select class="form-control" id="sleepTime" name="question_type_sleep_time" required aria-required="true">
+                                <option selected disabled hidden value="">----</option>
                                 <option value="answer-sleep-time-less-4">less than 4 hours</option>
                                 <option value="answer-sleep-time-4-to-6">between 4 and 6 hours</option>
                                 <option value="answer-sleep-time-6-to-8">between 6 and 8 hours</option>
@@ -24,8 +24,8 @@
 
                         <div class="form-group">
                             <label for="sleepQuality">I would say my sleep quality was...</label>
-                            <select class="form-control" id="sleepQuality" name="question_type_sleep_quality">
-                                <option selected disabled hidden>----</option>
+                            <select class="form-control" id="sleepQuality" name="question_type_sleep_quality" required aria-required="true">
+                                <option selected disabled hidden value="">----</option>
                                 <option value="answer-sleep-quality-excellent">excellent</option>
                                 <option value="answer-sleep-quality-good">good</option>
                                 <option value="answer-sleep-quality-fine">fine</option>
@@ -36,8 +36,8 @@
 
                         <div class="form-group">
                             <label for="difficultyWakingUp">Waking up was...</label>
-                            <select class="form-control" id="difficultyWakingUp" name="question_type_difficulty_waking_up">
-                                <option selected disabled hidden>----</option>
+                            <select class="form-control" id="difficultyWakingUp" name="question_type_difficulty_waking_up" required aria-required="true">
+                                <option selected disabled hidden value="">----</option>
                                 <option value="answer-difficulty-waking-up-very-easy">very easy</option>
                                 <option value="answer-difficulty-waking-up-easy">easy</option>
                                 <option value="answer-difficulty-waking-up-fine">fine</option>
