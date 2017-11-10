d6f960b1fadd16f157155952d9cea22e1ca24ad1
Helena <helena.wentworth@gmail.com>
2017-11-09 23:42:47 -0500

    TODO test cases

---
+++ tests/Feature/SurveyTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SurveyTest extends TestCase {
+    // TODO: Test case where no answers are submitted
+    // TODO: Test case where all answers are submitted
+    // TODO: Test case where some answers are submitted
+}
